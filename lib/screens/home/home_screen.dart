f16fa1e85bf7468ff365b41b760d534c25f996b8
Abhinav Singh <abhinavsingh22@hotmail.com>
2020-06-08 18:54:28 +0530

    screens/home: Add widget for home screen.

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:path_finder/routes.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          padding: EdgeInsets.all(10),
+          crossAxisSpacing: 5,
+          children: <Widget>[
+            GestureDetector(
+              child: Card(
+                child: Center(
+                  child: Text(
+                    'DFS',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                elevation: 5,
+              ),
+              onTap: () => Navigator.of(context).pushNamed(Routes.DFS),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
